37f30c84a7a5d9d043b7f068d5ec54dc5158d4df
Omidmo2 <omidvidoji@gmail.com>
2026-09-19 02:24:02 +0330

    Add WP Kandoo Mobile v1.0.0

---
+++ wp-kandoo-mobile/uninstall.php
@@ -0,0 +1,4 @@
+<?php
+if (!defined('WP_UNINSTALL_PLUGIN')) exit;
+// Intentionally preserves settings/data on uninstall to protect user configuration.
+// Administrators can use the Reset control before uninstalling if they want defaults.
